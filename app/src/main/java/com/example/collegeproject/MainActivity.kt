0bf9ea8c658d96665379db40e964664ce7c53c95
dirlinkf <vladimir.pavlenko098@gmail.com>
2021-04-02 02:33:53 +0700

    RemakeAdapter

--- app/src/main/java/com/example/collegeproject/MainActivity.kt
+++ app/src/main/java/com/example/collegeproject/MainActivity.kt
@@ -2,29 +2,35 @@ package com.example.collegeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
+import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ListView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
-
-
-class MainActivity : AppCompatActivity(){
+import kotlinx.android.synthetic.main.single_recyclerview_item.*
+import kotlinx.android.synthetic.main.single_recyclerview_item.view.*
+class MainActivity() : AppCompatActivity(){
 
     private lateinit var auth: FirebaseAuth
     private lateinit var user: FirebaseUser
     private lateinit var listUserTask: ListView
-    private lateinit var database: DatabaseReference
-    private lateinit var listString : List<String>
-    private val taskArr = mutableListOf<String>()
-    private lateinit var taskArrayAdapter:ArrayAdapter<String>
-
+    private lateinit var reference: DatabaseReference
+    private lateinit var checkRef: DatabaseReference
+    private lateinit var id:String
+    private lateinit var getCheckReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         taskRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -32,6 +38,10 @@ class MainActivity : AppCompatActivity(){
         /* Получние объекта аутентификации и получение текущего авторизованного пользователя */
         auth = FirebaseAuth.getInstance()
         user = auth.currentUser
+        reference = FirebaseDatabase.getInstance().reference.child(user.uid)
+
+
+
 
         /*       Получение ссылок на кнопки     */
         val addButton = findViewById<View>(R.id.addButton)
@@ -42,43 +52,18 @@ class MainActivity : AppCompatActivity(){
         addButton.setOnClickListener { addTaskButton() }
         deleteButton.setOnClickListener { deleteTaskButton() }
         clearButton.setOnClickListener { clearTaskButton() }
-
-        /*  Массив и его адаптер */
-        taskArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, taskArr)
-
-
-        /* Извлечение задач из базы данных.  */
-        var getdata = FirebaseDatabase.getInstance().reference.child(user.getUid()).child("tasks")
-        var getDataListener = object : ValueEventListener{
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                taskArr.clear()
-               snapshot.children.forEach {
-                    val keyMap: String = it.key.toString()
-                    taskArr.add(keyMap)
-                    taskArrayAdapter.notifyDataSetChanged()
-                }
-                taskArr.reverse()
-                taskRecyclerView.adapter = CustomRecyclerAdapter(taskArr)
-            }
-        }
-        getdata.addValueEventListener(getDataListener)
     }
-
 
     /* Функции кнопок, которые вызываются выше */
     fun addTaskButton(){
-        val taskName = taskTextField.text.toString()
-        val taskTime = taskTimeTextField.text.toString().toInt()
-        val isDone = false;
+        id = reference.push().key.toString()
+        val taskNameField = taskTextField.text.toString()
+        val taskTimeField = taskTimeTextField.text.toString()
+        val isDone = false
 
 
-        val task = ToDoModel(taskName, taskTime, isDone)
-        database = FirebaseDatabase.getInstance().reference.child(user.getUid()).child("tasks").push()
-        database.setValue(task)
+        val task = ToDoModel(id, taskNameField, taskTimeField)
+        reference.child(id).setValue(task)
     }
 
     fun deleteTaskButton(){
@@ -88,8 +73,61 @@ class MainActivity : AppCompatActivity(){
     fun clearTaskButton(){
 
     }
+
+
+   @Override
+    override fun onStart(){
+        super.onStart()
+        val options = FirebaseRecyclerOptions.Builder<ToDoModel>()
+                .setQuery(reference, ToDoModel::class.java)
+                .build()
+
+        val adapter = object : FirebaseRecyclerAdapter<ToDoModel, MyViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+                return MyViewHolder(LayoutInflater.from(parent.context)
+                        .inflate(R.layout.single_recyclerview_item, parent, false))
+            }
+
+            override fun onBindViewHolder(holder: MyViewHolder, position: Int, model: ToDoModel) {
+                holder.setTask(model.taskName)
+                holder.setChecked(isTaskDone = model.isDone, id = model.id)
+                holder.update(id = model.id, reference = reference)
+            }
+
+        }
+        taskRecyclerView.adapter = adapter
+        adapter.startListening()
+    }
+
+
+    private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun setTask(taskName : String){
+            val taskTextView: TextView = itemView.textView
+            taskTextView.text = taskName
+        }
+
+        fun setChecked(isTaskDone: String, id: String){
+            val isDoneCheckbox: CheckBox = itemView.findViewById(R.id.checkBox)
+            if (isDoneCheckbox != null) {
+                isDoneCheckbox.isChecked = isTaskDone.toBoolean()
+                isDoneCheckbox.tag = id
+
+            }
+        }
+        fun update(id:String, reference: DatabaseReference){
+            val isDoneCheckbox: CheckBox = itemView.findViewById(R.id.checkBox)
+            if (isDoneCheckbox != null) {
+                isDoneCheckbox.tag = id
+                isDoneCheckbox.setOnClickListener(){
+                    reference.child(isDoneCheckbox.tag.toString()).child("done").setValue(isDoneCheckbox.isChecked.toString())
+                }
+            }
+        }
+
+    }
+
+
 }
 
-interface OnClickItemView{
-    fun updateCheckBox(id: String, hasChecked: Boolean)
-}
+
--- app/src/main/java/com/example/collegeproject/ToDoModel.kt
+++ app/src/main/java/com/example/collegeproject/ToDoModel.kt
@@ -1,5 +1,8 @@
 package com.example.collegeproject
 
- data class ToDoModel(var taskName: String,var taskTime:Int,  var isDone:Boolean)
+data class ToDoModel(var id: String ="", var taskName: String="", var taskTime: String="", var isDone: String = "false")
+
+
+
 
 
